a02d757a4a7714e4110dd876b6409cad957b8816
Beglov Sergey <beglov.sergey@yandex.ru>
2023-04-12 21:49:52 +0300

    ChatGPT version

--- bot.rb
+++ bot.rb
@@ -1,14 +1,32 @@
 require 'telegram/bot'
+require 'openai'
 
 token = ENV.fetch('TELEGRAM_BOT_API_TOKEN')
+OpenAI.api_key = ENV.fetch('OPENAI_API_KEY')
+
+def handle_message(bot, message)
+  # Get the user's input text
+  input_text = message.text
+
+  # Generate a response using ChatGPT
+  response = OpenAI.Completion.create(
+    engine: 'davinci',
+    prompt: input_text,
+    max_tokens: 50,
+    n: 1,
+    stop: nil,
+    temperature: 0.5
+  )
+
+  # Send the response back to the user
+  bot.api.send_message(
+    chat_id: message.chat.id,
+    text: response.choices[0].text
+  )
+end
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    end
+    handle_message(bot, message)
   end
 end
